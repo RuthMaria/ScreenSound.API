6686db9bab168960f8807efd4d0d67d12086d256
RuthMaria <ruth-15rms@hotmail.com>
2025-01-06 22:14:38 -0300

    feat: adiciona migration para a tabela de genero

--- ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using ScreenSound.Modelos;
+using ScreenSound.Shared.Models.Modelos;
 
 namespace ScreenSound.Banco;
 
@@ -15,6 +16,8 @@ public class ScreenSoundContext: DbContext
 {
     public DbSet<Artista> Artistas { get; set; }
     public DbSet<Musica> Musicas { get; set; }
+    public DbSet<Genero> Genero { get; set; }
+
 
 
     private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
